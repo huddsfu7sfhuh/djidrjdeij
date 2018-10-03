5956da52989586edc1b627b7f09d5822c8708796
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-10-03 17:31:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -301,4 +301,38 @@ client.on('message', message => {
     }
 });
 
+client.on('ready', () => {
+ console.log("welcome bot"); 
+console.log("log");
+});
+
+client.on('ready', async () => {
+    let rSpeed = 4; // سرعة الرينبو , انا حاطها 2 يعني كل ثانيتين يغير لون الرتبة
+    let rGuild = '487956113694654464'; // اي دي السيرفر 
+    let rRole = '496290516866957314'; // اي دي الرتبة تجيبه عن طريق انك تفعل خاصية ان اي واحد يقدر يمنشن الرتبة و تمنشن الرتبة بالشات وقبل ما تمنشنها حط \
+    /* مثال :
+    \@TestingRole
+    الناتج :
+    <@&12345678987654321>
+    */
+    let rActive = true; /*
+    لتفعيل الرينبو حطها : true
+    لألغاء الرينبو حطها : false
+    */
+  rainbow(rSpeed, rGuild, rRole, rActive);
+  function rainbow(speed, guildid, roleid, enabled) {
+    if(enabled !== true && enabled !== false) throw new Error("SyntaxError: Rainbow enabled state must be true or false.");
+    if(enabled === false) return;
+    let guild = client.guilds.get(guildid);
+    let role = guild.roles.get(roleid);
+    let changeSpeed = speed * 1000;
+
+    setInterval(() => {
+      role.edit({
+        color: 'RANDOM'
+      });
+    }, changeSpeed);
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
